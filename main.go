dece3242ee2982d77cb220ff9a7557dac6ce2a3b
mrajkumar21 <rajmsd019@gmail.com>
2021-10-01 13:12:25 +0530

    initcommit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
+func main() {
+	http.HandleFunc("/hello", hello)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
